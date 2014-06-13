c060bc0e8968f8d580676d1f4a54a5ac86c8c62c
Max Ermolaev <emm531@gmail.com>
2014-06-13 17:15:23 +0400

    tasks

--- ajax/tasks.php
+++ ajax/tasks.php
@@ -13,6 +13,48 @@ if (isset($_GET['action'])) {
     }
 }
 
+
+if ($action == 'all') {
+
+    echo '
+<h1 class="page-header">Все задачи</h1>
+<div class="table-responsive">
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>#</th>
+            <th>Заказчик</th>
+            <th>Задача</th>
+            <th>Стоимость</th>
+        </tr>
+        </thead>
+        <tbody>';
+    $q = mysqli_query($main_db, 'SELECT
+                `tasks`.`id` AS `id`,
+                `tasks`.`author` AS `author_id`,
+                `authors`.`name` AS `author_name`,
+                `tasks`.`title` AS `title`,
+                `tasks`.`description` AS `description`,
+                `tasks`.`cost` AS `cost`
+            FROM `tasks`
+            INNER JOIN `users` AS `authors` ON `authors`.`id`=`tasks`.`author`
+            WHERE `tasks`.`performer`=0
+            ORDER BY `tasks`.`id` DESC');
+
+    while ($task = mysqli_fetch_assoc($q)) {
+        echo '<tr>
+                <td>' . $task['id'] . '</td>
+                <td>' . $task['author_name'] . '</td>
+                <td>' . $task['title'] . '</td>
+                <td>' . $task['cost'] . '</td>
+            </tr>';
+    }
+    echo '
+        </tbody>
+    </table>
+</div>';
+}
+
 if ($action == 'my') {
 
     echo '
