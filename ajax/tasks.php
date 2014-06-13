4ae2e0bada1ea549682ee328972ec2b994a09d05
Max Ermolaev <emm531@gmail.com>
2014-06-13 17:08:22 +0400

    tasks

--- ajax/tasks.php
+++ ajax/tasks.php
@@ -1,51 +1,57 @@
 <?php
-
+session_start();
 require_once('../lib/mysql.php');
+require_once('../lib/auth.php');
 
-?>
+$action = 'all';
+if (isset($_GET['action'])) {
+    if ($_GET['action'] == 'all') {
+        $action = 'all';
+    }
+    if ($_GET['action'] == 'my' && isset($user)) {
+        $action = 'my';
+    }
+}
 
-<h1 class="page-header">Общие задачи</h1>
+if ($action == 'my') {
 
+    echo '
+<h1 class="page-header">Мои задачи</h1>
 <div class="table-responsive">
     <table class="table table-striped">
         <thead>
         <tr>
             <th>#</th>
             <th>Заказчик</th>
+            <th>Исполнитель</th>
             <th>Описание</th>
             <th>Стоимость</th>
         </tr>
         </thead>
-        <tbody>
-
-        <?php
-
-        $q = mysqli_query($main_db, 'SELECT
+        <tbody>';
+    $q = mysqli_query($main_db, 'SELECT
                 `tasks`.`id` AS `id`,
                 `tasks`.`author` AS `author_id`,
-                `users`.`name` AS `author_name`,
+                `authors`.`name` AS `author_name`,
                 `tasks`.`title` AS `title`,
                 `tasks`.`description` AS `description`,
                 `tasks`.`cost` AS `cost`
             FROM `tasks`
-            INNER JOIN `users` ON `users`.`id`=`tasks`.`author`
-            WHERE `tasks`.`performer`=0
+            INNER JOIN `users` AS `authors` ON `authors`.`id`=`tasks`.`author`
+            WHERE `tasks`.`author`='.$user['id'].' OR `tasks`.`performer`='.$user['id'].'
             ORDER BY `tasks`.`id` DESC');
 
-        while ($task = mysqli_fetch_assoc($q)) {
-
-            echo '<tr>
+    while ($task = mysqli_fetch_assoc($q)) {
+        echo '<tr>
                 <td>' . $task['id'] . '</td>
+                <td>' . $task['author_name'] . '</td>
                 <td>' . $task['author_name'] . '</td>
                 <td>' . $task['title'] . '</td>
                 <td>' . $task['cost'] . '</td>
             </tr>';
-
-        }
-
-        ?>
-
-
+    }
+    echo '
         </tbody>
     </table>
-</div>
+</div>';
+}
