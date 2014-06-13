c8b78b03fc840455e5aba609d9e0cf6c77bae2ee
Max Ermolaev <emm531@gmail.com>
2014-06-13 13:19:14 +0400

    ajax menu

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
+
+$.ajax({
+    url: "ajax/menu.php"
+}).done(function (data) {
+    $('#menu').html(data);
+});
+
 $('#tasks_list').click(function () {
     $.ajax({
         url: "ajax/tasks.php"
     }).done(function (data) {
         $('#content').html(data);
     });
-});
-
-$.ajax({
-    url: "ajax/menu.php"
-}).done(function (data) {
-    $('#menu').html(data);
 });
