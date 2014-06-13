09dc8bd4138d6181ce4d065c7ad6d141a02102a0
Max Ermolaev <emm531@gmail.com>
2014-06-13 14:01:54 +0400

    auth

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 require_once('lib/mysql.php');
+require_once('lib/auth.php');
 ?>
 
 <!DOCTYPE html>
---
+++ lib/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+if (isset($_SESSION['uid']) || isset($_GET['uid'])){
+
+    if (isset($_SESSION['uid'])){
+        $uid=intval($_SESSION['uid']);
+    }
+    if (isset($_GET['uid'])){
+        $uid=intval($_GET['uid']);
+    }
+
+    $q=mysqli_query($main_db, 'SELECT * FROM `users` WHERE `id`='.$uid.' LIMIT 1');
+
+    if (mysqli_num_rows($q)>0){
+        $user=mysqli_fetch_assoc($q);
+    }
+
+}
+
+print_r($user);
