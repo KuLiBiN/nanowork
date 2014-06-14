ab4acff7959d69cba5a06d9989c8b2670712d172
Max Ermolaev <emm531@gmail.com>
2014-06-14 19:20:49 +0400

    tasks

--- ajax/tasks.php
+++ ajax/tasks.php
@@ -13,6 +13,13 @@ if (isset($_GET['action'])) {
     }
 }
 
+// имена пользователей
+$users_info = array();
+$users_info[0] = '';
+$q = mysqli_query($main_db, 'SELECT `id`, `name` FROM `users`');
+while ($u = mysqli_fetch_assoc($q)) {
+    $users_info[$u['id']] = $u['name'];
+}
 
 if ($action == 'all') {
 
@@ -57,43 +64,46 @@ if ($action == 'all') {
 
 if ($action == 'my') {
 
-    echo '
+    // заказчик
+    if ($user['type'] == 1) {
+
+        echo '
 <h1 class="page-header">Мои задачи</h1>
+
+
+
 <div class="table-responsive">
     <table class="table table-striped">
         <thead>
         <tr>
             <th>#</th>
-            <th>Заказчик</th>
             <th>Исполнитель</th>
             <th>Описание</th>
             <th>Стоимость</th>
         </tr>
         </thead>
         <tbody>';
-    $q = mysqli_query($main_db, 'SELECT
-                `tasks`.`id` AS `id`,
-                `tasks`.`author` AS `author_id`,
-                `authors`.`name` AS `author_name`,
-                `tasks`.`title` AS `title`,
-                `tasks`.`description` AS `description`,
-                `tasks`.`cost` AS `cost`
-            FROM `tasks`
-            INNER JOIN `users` AS `authors` ON `authors`.`id`=`tasks`.`author`
-            WHERE `tasks`.`author`='.$user['id'].' OR `tasks`.`performer`='.$user['id'].'
-            ORDER BY `tasks`.`id` DESC');
+        $q = mysqli_query($main_db, 'SELECT `id`, `title`, `performer`
+        FROM `tasks` WHERE `author`=' . $user['id'] . '
+        ORDER BY `id` DESC');
 
-    while ($task = mysqli_fetch_assoc($q)) {
-        echo '<tr>
+        while ($task = mysqli_fetch_assoc($q)) {
+            $work = 0;
+            if ($task['performer'] > 0) {
+                $work = 1;
+            }
+            echo '<tr class="work'.$work.'">
                 <td>' . $task['id'] . '</td>
-                <td>' . $task['author_name'] . '</td>
-                <td>' . $task['author_name'] . '</td>
+                <td>' . $users_info[$task['id']] . '</td>
                 <td>' . $task['title'] . '</td>
                 <td>' . $task['cost'] . '</td>
             </tr>';
-    }
-    echo '
+        }
+        echo '
         </tbody>
     </table>
 </div>';
+    }
+
+
 }
