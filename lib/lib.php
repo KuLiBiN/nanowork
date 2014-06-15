a70fce74c02aa0f63adf7bd940d321fa9ca0c5cb
Max Ermolaev <emm531@gmail.com>
2014-06-15 20:46:23 +0400

    users

--- lib/lib.php
+++ lib/lib.php
@@ -1,3 +1,8 @@
 <?php
 
-$system_percent=10; // процент системы
+$system_percent = 10; // процент системы
+
+// типы юзеров
+$user_type = array();
+$user_type[1] = 'Заказчик';
+$user_type[2] = 'Исполнитель';
--- lib/menu.php
+++ lib/menu.php
@@ -5,11 +5,13 @@ if (!isset($user)) {
     <ul class="nav navbar-nav navbar-right">
         <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">Авторизация <b class="caret"></b></a>
-            <ul class="dropdown-menu">
-                <li><a href="?uid=1">Заказчик</a></li>
-                <li><a href="?uid=3">Исполнитель</a></li>
-                <li><a href="#">Администратор</a></li>
-            </ul>
+            <ul class="dropdown-menu">';
+            // вывод юзеров
+            $q = mysqli_query($main_db, 'SELECT `id`, `type`, `name` FROM `users` ORDER BY `type` ASC, `name` ASC');
+            while ($u = mysqli_fetch_assoc($q)) {
+                echo '<li><a href="?uid=' . $u['id'] . '">' . $user_type[$u['type']] . ' ' . $u['name'] . '</a></li>';
+            }
+            echo '</ul>
         </li>
     </ul>';
 
@@ -18,7 +20,7 @@ if (!isset($user)) {
         echo '
     <ul class="nav navbar-nav">
         <li><a href="#my_tasks" id="my_tasks">Мои задачи</a></li>
-        <li><a href="#">У вас на счету <span id="money">'.$user['money'].'</span> руб.</a></li>
+        <li><a href="#">У вас на счету <span id="money">' . $user['money'] . '</span> руб.</a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
         <li class="dropdown">
@@ -35,7 +37,7 @@ if (!isset($user)) {
     <ul class="nav navbar-nav">
         <li><a href="#all_tasks" id="all_tasks">Все задачи</a></li>
         <li><a href="#my_tasks" id="my_tasks">Мои задачи</a></li>
-        <li><a href="#">У вас на счету <span id="money">'.$user['money'].'</span> руб.</a></li>
+        <li><a href="#">У вас на счету <span id="money">' . $user['money'] . '</span> руб.</a></li>
     </ul>
    <ul class="nav navbar-nav navbar-right">
         <li class="dropdown">
